9994b73b9d2e89f3c3e90b24f2df4a17b018591c
ManuelWestermeier <westermeier111@gmail.com>
2024-10-16 16:55:59 +0200

    add webview render

---
+++ src/js/data.js
@@ -0,0 +1,20 @@
+const defaultData = {
+    tabs: [
+        ["https://manuelwestermeier.github.io/fsRch/", null],
+        ["https://www.youtube.com/@TAGESSCHATTEN", null],
+        ["https://manuelwestermeier.github.io/memory_game", null]
+    ], searchEngine: "https://manuelwestermeier.github.io/fsRch/"
+}
+
+function getData() {
+    const data = localStorage.getItem("browser-data")
+
+    try {
+        return data ? JSON.parse(data) : defaultData
+    } catch (error) {
+        console.error(error)
+        return defaultData
+    }
+}
+
+module.exports = getData()
--- src/js/index.js
+++ src/js/index.js
@@ -1,4 +1,49 @@
 // html elems
 const { controllsElem, webViewsElem } = require("./js/elems.js")
+//app state data
+const data = require("./js/data.js")
 // toggle menu
 require("./js/key-down.js")
+
+function createWebview([url = data.searchEngine, metaData = null]) {
+    const webviewElem = document.createElement("webview")
+
+    webviewElem.src = url
+
+    return webviewElem
+}
+
+function createNewTab(pos, tabData = [data.searchEngine, null]) {
+    //no items
+    if (pos < 0 || data.tabs.length == 0) {
+        data.tabs.push(tabData)
+        webViewsElem.append(createWebview(tabData))
+        return
+    }
+
+    if (webViewsElem.children.length == 0) {
+        webViewsElem.append(createWebview(tabData))
+        return
+    }
+
+    const refElem = webViewsElem.children[pos];
+    // Insert the new element before the element at position 2
+    webViewsElem.insertBefore(createWebview(tabData), refElem);
+}
+
+function checkZeroTabs() {
+    if (data.tabs.length == 0) {
+        const tabData = [data.searchEngine, null]
+        createNewTab(-1, tabData)
+    }
+}
+
+function renderAllWebViewFromStart() {
+    checkZeroTabs()
+    webViewsElem.innerHTML = ""
+    for (let index = 0; index < data.tabs.length; index++) {
+        createNewTab(index, data.tabs[index])
+    }
+}
+
+renderAllWebViewFromStart()
