b88267c0cd182ffae0143894f3876b5a0f9d0c5a
ManuelWestermeier <westermeier111@gmail.com>
2024-10-19 01:05:15 +0200

    history

--- src/js/render-tabs.js
+++ src/js/render-tabs.js
@@ -29,6 +29,7 @@ function removeTab(index) {
 }
 
 function createTabElem([url = data.searchEngine, metaData = null], index) {
+    const webview = webViewsElem.children[index]
     const tabElem = document.createElement("div")
     tabElem.setAttribute("tabindex", "0");
     tabElem.classList.add("tab")
@@ -38,9 +39,22 @@ function createTabElem([url = data.searchEngine, metaData = null], index) {
         tabElem.scrollIntoView({ behavior: "smooth", block: "center" })
     }
 
-    tabElem.innerHTML = `<span></span> <button>close</button>`
+    tabElem.innerHTML = `
+    <span></span>
+    <button>close</button>
+    <div class="controll">
+        <button title="back">
+            <svg xmlns="http://www.w3.org/2000/svg" height="24px" viewBox="0 -960 960 960" width="24px" fill="#e8eaed"><path d="m313-440 224 224-57 56-320-320 320-320 57 56-224 224h487v80H313Z"/></svg>
+        </button>
+        <button title="reload">
+            <svg xmlns="http://www.w3.org/2000/svg" height="24px" viewBox="0 -960 960 960" width="24px" fill="#e8eaed"><path d="M480-160q-134 0-227-93t-93-227q0-134 93-227t227-93q69 0 132 28.5T720-690v-110h80v280H520v-80h168q-32-56-87.5-88T480-720q-100 0-170 70t-70 170q0 100 70 170t170 70q77 0 139-44t87-116h84q-28 106-114 173t-196 67Z"/></svg>
+        </button>
+        <button title="forward">
+            <svg xmlns="http://www.w3.org/2000/svg" height="24px" viewBox="0 -960 960 960" width="24px" fill="#e8eaed"><path d="M647-440H160v-80h487L423-744l57-56 320 320-320 320-57-56 224-224Z"/></svg>
+        </button>
+    </div>`
 
-    const [tabTextElem, tabCloseButton] = tabElem.children
+    const [tabTextElem, tabCloseButton, controlls] = tabElem.children
 
     tabTextElem.innerText = url
     tabTextElem.onclick = () => {
@@ -48,15 +62,30 @@ function createTabElem([url = data.searchEngine, metaData = null], index) {
     }
 
     tabElem.onkeydown = ({ key }) => {
+        //open close
         if (key == "Enter" || key == "y")
             openTab(index)
         else if (key == "x")
             removeTab(index)
+
+        //history
+        else if (key == "r")
+            webview.reload()
+        else if (key == "f")
+            webview.forward()
+        else if (key == "b")
+            webview.back()
     }
 
     tabCloseButton.onclick = () => {
         removeTab(index)
     }
+
+    const [backWardsButton, reloadButton, forwardsButton] = controlls.children
+
+    backWardsButton.onclick = () => webview.back()
+    reloadButton.onclick = () => webview.reload()
+    forwardsButton.onclick = () => webview.forward()
 
     return tabElem
 }
